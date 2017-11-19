f36ce1bfd9c2fbe3421039962133dd5ce9ad5927
PhamKimDat <kimdat171296@gmail.com>
2017-11-19 10:52:17 +0700

    Update TestTachChuoi.java
    Update code in TestTachChuoi

--- src/test/TestTachChuoi.java
+++ src/test/TestTachChuoi.java
@@ -1,64 +1,48 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 
 import org.junit.Test;
 
 import GUI.ManHinhQuanLyHangHoa;
 
 public class TestTachChuoi {
-
-	// ham tach chuoi ben trong, vd: love is {beautiful} => beautiful
-
-	// kiem tra ko hop le
-	@Test
-	public void testNull() {
-		String s = "beautiful";
-		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi(""));
-	}
-	// ham tach chuoi ben trong, vd: love is {beautiful} => beautiful
-
-	// kiem tra ko hop le(chuoi trong hoac khong ton tai dau {})
+	
+	//ham tach chuoi ben trong, vd: love is {beautiful} => beautiful
+	
+	//kiem tra ko hop le(chuoi trong hoac khong ton tai dau {})
 	@Test
 	public void testChuoiTrong() {
-		String s = "beautiful";
-		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi(""));
-	}
+			String s="beautiful";
+			assertNotEquals(s, ManHinhQuanLyHangHoa.tachChuoi("{  }"));
 
+	}
+	
 	@Test
-	public void testNotContain() {
-		String s = "beautiful";
-		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is beautiful"));
-	}
-
 	public void testKhongTonTai() {
-		String s = "beautiful";
-		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is beautiful"));
-	}
+			String s="beautiful";
+			assertNotEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love {is} beautiful"));
 
+	}
+	
 	@Test
-	public void testNotContain_1() {
-		String s = "beautiful";
-		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is [beautiful] "));
-	}
-
 	public void testKhongTonTai_1() {
-		String s = "beautiful";
-		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is [beautiful] "));
+			String s="beautiful";
+			assertNotEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is {[beautiful]} "));
 
 	}
-
-	// kiem tra hop le
+	
+	//kiem tra hop le
 	@Test
-	public void testContain() {
-		String s = "beautiful";
-		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is {beautiful} "));
-	}
-
 	public void testTonTai() {
-		String s = "beautiful";
-		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is {beautiful} "));
+			String s="beautiful";
+			assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is {beautiful} "));
 
 	}
-
+	
+	
+	
+	
 }
