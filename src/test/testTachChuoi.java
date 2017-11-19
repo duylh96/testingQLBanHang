fc2ae3d025e6de3e6801b5143174e48379ead4d8
Hoang Duy <duylh.1996@gmail.com>
2017-11-19 10:25:16 +0700

    Justify

--- src/test/testTachChuoi.java
+++ src/test/testTachChuoi.java
@@ -1,47 +1,43 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.Test;
 
 import GUI.ManHinhQuanLyHangHoa;
 
 public class testTachChuoi {
-	
-	//ham tach chuoi ben trong, vd: love is {beautiful} => beautiful
-	
-	//kiem tra ko hop le
+
+	// ham tach chuoi ben trong, vd: love is {beautiful} => beautiful
+
+	// kiem tra ko hop le
 	@Test
 	public void testNull() {
-			String s="beautiful";
-			assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi(""));
+		String s = "beautiful";
+		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi(""));
 
 	}
-	
+
 	@Test
 	public void testNotContain() {
-			String s="beautiful";
-			assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is beautiful"));
+		String s = "beautiful";
+		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is beautiful"));
 
 	}
-	
+
 	@Test
 	public void testNotContain_1() {
-			String s="beautiful";
-			assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is [beautiful] "));
+		String s = "beautiful";
+		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is [beautiful] "));
 
 	}
-	
-	//kiem tra hop le
+
+	// kiem tra hop le
 	@Test
 	public void testContain() {
-			String s="beautiful";
-			assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is {beautiful} "));
+		String s = "beautiful";
+		assertEquals(s, ManHinhQuanLyHangHoa.tachChuoi("love is {beautiful} "));
 
 	}
-	
-	
-	
-	
+
 }
